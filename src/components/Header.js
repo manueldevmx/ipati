a5d0f6d3494b71936159df52ff753eb8686a29fe
manueldev_codemx <43160136+manuelcode-dev@users.noreply.github.com>
2020-10-17 15:52:47 -0500

    Styles
    header - footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark  justify-content-between">
             <div className="container">
-                <h1><Link to={'/'} className="text-light">i-Pati: La API con el valor de Mexico</Link></h1>
+                <h1><Link to={'/'} className="text-light">Base de Datos Publica de Mexico</Link></h1>
             </div>
 
             <Link to={"/productos/nuevo"}
